85d5665f6005cde19aa42639c55d6e908e1c904d
Shahin Zangenehpour <98chimp@users.noreply.github.com>
2015-03-27 22:03:39 -0700

    Initial Commit

---
+++ Insta.Kilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Insta.Kilo
+//
+//  Created by Shahin on 2015-03-27.
+//  Copyright (c) 2015 98% Chimp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Insta.Kilo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Insta.Kilo
+//
+//  Created by Shahin on 2015-03-27.
+//  Copyright (c) 2015 98% Chimp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
